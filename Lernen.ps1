fffd2cac392742ffb248ba7be401a3fd472fc6a9
vinpuch <94136786+vinpuch@users.noreply.github.com>
2023-01-22 17:54:23 +0100

    Add files via upload

---
+++ Lernen.ps1
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+start "C:\Program Files\Google\Chrome\Application\chrome.exe"
+start "C:\Users\vince\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Discord Inc\Discord.lnk"
+
+start "C:\Program Files\Anki\anki.exe"
+
+
+
+msg * "Viel Erfolg beim Lernen!"
